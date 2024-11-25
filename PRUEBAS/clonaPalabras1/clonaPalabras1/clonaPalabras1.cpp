0519943aca6a788ede5303d731563b00cabb7283
JesusSevilla30 <jesussevilla3006@gmail.com>
2024-11-25 12:58:25 -0600

    octavo commit

---
+++ PRUEBAS/clonaPalabras1/clonaPalabras1/clonaPalabras1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#define TAMTOKEN 30O
+
+int main()
+{
+    char* szPalabraLeida,
+        char    szPalabrasSugeridas[][TAMTOKEN];
+    int& iNumSugeridas);
+        
+            int longitud, posicion = 0;
+            int i, j, m, jPalabra = 0, apuntador, jApuntador, contador;
+            int posicion2 = 0;
+
+            char letras[] = { "abcdefghijklmnopqrstuvwxyz" };
+            char letras2[] = { "ñáéíóú" };
+
+            
+            char szPalabraLeidaCopia[TAMTOKEN];
+            char szPalabraLeidaCopia2[TAMTOKEN];
+            char palabrafin[TAMTOKEN];
+            char auxiliarPal[TAMTOKEN];
+            char auxiliarPal2[TAMTOKEN];
+           
+}
+
